5a0b69b354a9b00d7e189dad3a52d2d8320a31e2
Jordan Evens <jordan.evens@canada.ca>
2022-06-06 14:41:27 -0400

    reformatted

--- util.h
+++ util.h
@@ -10,7 +10,7 @@ const float pi;
 
 float findQ(float temp, float rh);
 float findrh(float q, float temp);
-float sun(float lat,float lon, int mon,int day,int hour, int timezone, float *sunrise, float *sunset);
-float sun_julian(float lat,float lon, int jd,int hour,int timezone, float *sunrise, float *sunset);
+float sun(float lat, float lon, int mon, int day, int hour, int timezone, float *sunrise, float *sunset);
+float sun_julian(float lat, float lon, int jd, int hour, int timezone, float *sunrise, float *sunset);
 int julian(int mon, int day);
 #endif
