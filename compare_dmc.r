761aaeb28f82898ac8445c935d8590f7a8a9ea3c
Jordan Evens <jordan.evens@canada.ca>
2022-06-27 08:25:38 -0400

    changed to arrange plots for comparison

--- compare_dmc.r
+++ compare_dmc.r
@@ -1,3 +1,4 @@
+library("gridExtra")
 source("NG_FWI.r")
 source("NG_FWI_old.r")
 bak <- as.data.table(read.csv("./bak_hourly.csv"))
@@ -10,18 +11,18 @@ plot_compare <- function(df, main="")
   # compare old results to new results
   result_old[, TIMESTAMP := make_datetime(year, mon, day, hour)]
   result[, TIMESTAMP := make_datetime(year, mon, day, hour)]
-  print(ggplot() + ggtitle(main) + geom_line(mapping=aes(TIMESTAMP, DMC), data=result, linetype="dashed", color="red") + geom_line(mapping=aes(TIMESTAMP, DMC), data=result_old))
+  return(ggplot() + ggtitle(main) + geom_line(mapping=aes(TIMESTAMP, DMC), data=result, linetype="dashed", color="red") + geom_line(mapping=aes(TIMESTAMP, DMC), data=result_old))
 }
 
-plot_compare(bak, "Original")
 bak_3day <- bak[1:72]
 # bak_3day$rh <- 80
 # bak_3day$temp <- 20
 # bak_3day$wind <- 10
-plot_compare(bak_3day, "3 Day")
+p_3day <- plot_compare(bak_3day, "3 Day")
 bak_3day$rain <- 0.1
-plot_compare(bak_3day, "0.1mm/hr")
+p_0_1 <- plot_compare(bak_3day, "0.1mm/hr")
 bak_3day$rain <- 0.25
-plot_compare(bak_3day, "0.25mm/hr")
+p_0_25 <- plot_compare(bak_3day, "0.25mm/hr")
 bak_3day$rain <- 0.5
-plot_compare(bak_3day, "0.5mm/hr")
+p_0_5 <- plot_compare(bak_3day, "0.5mm/hr")
+grid.arrange(p_3day, p_0_1, p_0_25, p_0_5, nrow=2)
