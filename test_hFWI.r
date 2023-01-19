45073508bb803951448fd01c963824d9feecab67
Jordan Evens <jordan.evens@gmail.com>
2023-01-19 09:39:50 -0500

    add code from old cffdrs so we can eliminate terra dependency from importing library cffdrs

--- test_hFWI.r
+++ test_hFWI.r
@@ -1,8 +1,8 @@
 #' Plots a comparison between hourly and daily values
 source("NG_FWI.r")
+source("old_cffdrs.r")
 
-
-test_hfwi <- function(df=cffdrs::test_hffmc, timezone=-6)
+test_hfwi <- function(df=read.csv('./test_hffmc.csv'), timezone=-6)
 {
   # set up as if we had called hFWI
   weatherstream <- data.table(df)
@@ -16,16 +16,16 @@ test_hfwi <- function(df=cffdrs::test_hffmc, timezone=-6)
   d <- toDaily(w)
   d[, LAT := DEFAULT_LATITUDE]
   d[, LONG := DEFAULT_LONGITUDE]
-  daily <- cffdrs::fwi(d, init=c(FFMC_DEFAULT, DMC_DEFAULT, DC_DEFAULT))
+  daily <- fwi(d, init=c(FFMC_DEFAULT, DMC_DEFAULT, DC_DEFAULT))
   setnames(daily,
            c('FFMC', 'DMC', 'DC', 'ISI', 'BUI', 'FWI', 'DSR'),
            c('DFFMC', 'DDMC', 'DDC', 'DISI', 'DBUI', 'DFWI', 'DDSR'))
   r <- merge(r,
              daily[, c('YR', 'MON', 'DAY', 'DFFMC', 'DDMC', 'DDC', 'DISI', 'DBUI', 'DFWI', 'DDSR')],
              by=c('YR', 'MON', 'DAY'))
-  r[, DISI := cffdrs:::.ISIcalc(DFFMC, WS, fbpMod = FALSE)]
-  r[, DBUI := cffdrs:::.buiCalc(DDMC, DDC)]
-  r[, DFWI := cffdrs:::.fwiCalc(DISI, DBUI)]
+  r[, DISI := ISIcalc(DFFMC, WS, fbpMod = FALSE)]
+  r[, DBUI := BUIcalc(DDMC, DDC)]
+  r[, DFWI := FWIcalc(DISI, DBUI)]
   # taken from package code
   r[, DDSR := 0.0272 * (DFWI ^ 1.77)]
   # output input and FWI columns so git can tell us if they change
@@ -42,7 +42,7 @@ plot_comparison <- function(r)
   print(ggplot(r) + geom_line(aes(TIMESTAMP, DC)) + geom_point(aes(TIMESTAMP, DDC), data=r[hour(TIMESTAMP) == 16]))
 }
 
-plot_test <- function(df=cffdrs::test_hffmc)
+plot_test <- function(df=read.csv('./test_hffmc.csv'))
 {
   r <- test_hfwi(df)
   plot_comparison(r)
