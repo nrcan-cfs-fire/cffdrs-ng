3d3e8bd552a4777cf520a1d672f6fb0d964ceb53
Jordan Evens <jordan.evens@nrcan-rncan.gc.ca>
2023-10-06 10:09:56 -0400

    added cmake config

--- test.sh
+++ test.sh
@@ -1,14 +1,13 @@
-gcc -o ngfwi NG_FWI.c util.c -lm -std=c90 || exit
+cmake --configure . || exit
+cmake --build . || exit
+
 ./ngfwi -6 85 6 15 ./bak_hourly.csv test.csv
 ./ngfwi -6 85 6 15 ./input_hffmc.csv test2.csv
 
-gcc -o make_daily make_daily.c util.c -lm -std=c90 || exit
 ./make_daily ./bak_hourly.csv bak_daily_c.csv
 
-gcc -o make_minmax make_minmax.c util.c -lm -std=c90 || exit
 ./make_minmax ./bak_daily.csv bak_minmax_c.csv
 
-gcc -o make_hourly make_hourly.c util.c -lm -std=c90 || exit
 ./make_hourly -6 ./bak_minmax_c.csv bak_diurnal_c.csv
 
 ./ngfwi -6 85 6 15 ./bak_diurnal.csv test3.csv
