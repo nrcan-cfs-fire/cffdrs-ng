9f2cf681d00fc9b5c4bbc995593df1ee1aae7b9e
Jordan Evens <jordan.evens@nrcan-rncan.gc.ca>
2023-10-10 12:42:05 -0400

    clean up directory structure

--- plot_c.r
+++ plot_c.r
@@ -4,13 +4,13 @@ source("NG_FWI.r")
 library(ggplot2)
 library(data.table)
 library(lubridate)
-df_wx <- as.data.table(read.csv("./wx_hourly.csv"))
+df_wx <- as.data.table(read.csv("./data/wx_hourly.csv"))
 setnames(df_wx, c("year", "hour", "wind", "rain"), c("yr", "hr", "ws", "prec"))
 result <- test_hfwi(df_wx, timezone = -6)
 
 plot_comparison(result)
 
-wx_diurnal <- as.data.table(read.csv("./wx_diurnal.csv"))
+wx_diurnal <- as.data.table(read.csv("./data/wx_diurnal.csv"))
 setnames(wx_diurnal, c("year", "hour", "wind", "rain"), c("yr", "hr", "ws", "prec"))
 result3 <- test_hfwi(wx_diurnal, timezone = -6)
 
--- test_c.py
+++ test_c.py
@@ -54,11 +54,11 @@ def save_csv(df, file):
 
 
 def run_tests():
-    df_wx = pd.read_csv("./wx_hourly.csv")
+    df_wx = pd.read_csv("./data/wx_hourly.csv")
     result = NG_FWI.hFWI(df_wx, timezone=-6)
     save_csv(result, "./result_py.csv")
 
-    df_wx = pd.read_csv("./wx_hourly.csv")
+    df_wx = pd.read_csv("./data/wx_hourly.csv")
     test_hffmc = pd.read_csv("test_hffmc.csv")
     df = test_hffmc.copy()
     df["lat"] = df_wx["lat"].iloc[0]
@@ -66,13 +66,13 @@ def run_tests():
     COLUMN_SYNONYMS = {"yr": "year", "hr": "hour", "ws": "wind", "prec": "rain"}
     df = df.rename(columns=COLUMN_SYNONYMS)
     df = df[["lat", "long", "year", "mon", "day", "hour", "temp", "rh", "wind", "rain"]]
-    df.to_csv("./input_hffmc_py.csv", index=False)
-    df = pd.read_csv("./input_hffmc_py.csv")
+    df.to_csv("./data/test_hffmc_py.csv", index=False)
+    df = pd.read_csv("./data/test_hffmc_py.csv")
 
     result_hffmc = NG_FWI.hFWI(df, timezone=-6)
     save_csv(result_hffmc, "./result_hffmc_py.csv")
 
-    df_wx = pd.read_csv("./wx_hourly.csv")
+    df_wx = pd.read_csv("./data/wx_hourly.csv")
     df = make_daily.hourly_to_daily(df_wx)
     df["mon"] = df["mon"].apply("{:02d}".format)
     df["day"] = df["day"].apply("{:02d}".format)
@@ -82,9 +82,9 @@ def run_tests():
     df["wind"] = df["wind"].apply(round_format("{:.1f}", 1))
     df["rain"] = df["rain"].apply(round_format("{:.1f}", 1))
     df = df[["lat", "long", "year", "mon", "day", "hour", "temp", "rh", "wind", "rain"]]
-    df.to_csv("wx_daily_py.csv", index=False)
+    df.to_csv("./data/wx_daily_py.csv", index=False)
 
-    df_wx = pd.read_csv("./wx_daily_py.csv")
+    df_wx = pd.read_csv("./data/wx_daily_py.csv")
     df = make_minmax.daily_to_minmax(df_wx)
     df["mon"] = df["mon"].apply("{:02d}".format)
     df["day"] = df["day"].apply("{:02d}".format)
@@ -113,9 +113,9 @@ def run_tests():
             "rain",
         ]
     ]
-    df.to_csv("wx_minmax_py.csv", index=False)
+    df.to_csv("./data/wx_minmax_py.csv", index=False)
 
-    wx_minmax = pd.read_csv("./wx_minmax_py.csv")
+    wx_minmax = pd.read_csv("./data/wx_minmax_py.csv")
     df = make_hourly.minmax_to_hourly(wx_minmax, timezone=-6)
     # FIX: this is just to match R code for now
     df["year"] = df["year"].apply("{:02d}".format)
@@ -127,20 +127,22 @@ def run_tests():
     df["wind"] = df["wind"].apply(round_format("{:.1f}", 1))
     df["rain"] = df["rain"].apply(round_format("{:.1f}", 1))
     df = df[["lat", "long", "year", "mon", "day", "hour", "temp", "rh", "wind", "rain"]]
-    df.to_csv("wx_diurnal_py.csv", index=False)
+    df.to_csv("./data/wx_diurnal_py.csv", index=False)
 
-    wx_diurnal = pd.read_csv("./wx_diurnal_py.csv")
+    wx_diurnal = pd.read_csv("./data/wx_diurnal_py.csv")
     result3 = NG_FWI.hFWI(wx_diurnal, timezone=-6)
     save_csv(result3, "./result3_py.csv")
 
-    wx_windy = pd.read_csv("./wx_windy.csv")
+    wx_windy = pd.read_csv("./data/wx_windy.csv")
     result4 = NG_FWI.hFWI(wx_windy, timezone=-6)
     save_csv(result4, "result4_py.csv")
 
     save_csv(
-        NG_FWI.hFWI(pd.read_csv("./wx_rh100.csv"), timezone=-6), "./result5_py.csv"
+        NG_FWI.hFWI(pd.read_csv("./data/wx_rh100.csv"), timezone=-6), "./result5_py.csv"
     )
-    save_csv(NG_FWI.hFWI(pd.read_csv("./wx_rh0.csv"), timezone=-6), "./result6_py.csv")
+    save_csv(
+        NG_FWI.hFWI(pd.read_csv("./data/wx_rh0.csv"), timezone=-6), "./result6_py.csv"
+    )
 
 
 if __name__ == "__main__":
