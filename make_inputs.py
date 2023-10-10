088145ea4411ca9e5fb8dec75ae50e7707dd7b66
Jordan Evens <jordan.evens@nrcan-rncan.gc.ca>
2023-10-10 12:14:33 -0400

    added python script to generate extreme inputs from hourly inputs file for testing

---
+++ make_inputs.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+# make some extreme weather to test boundary conditions
+
+df = pd.read_csv("wx_hourly.csv")
+df["rh"] = df["rh"].apply(lambda x: max(0, x - 20))
+df.to_csv("wx_rh0.csv", index=False)
+
+df = pd.read_csv("wx_hourly.csv")
+df["rh"] = df["rh"].apply(lambda x: min(100, x * 3))
+df.to_csv("wx_rh100.csv", index=False)
+
+df = pd.read_csv("wx_hourly.csv")
+df["wind"] = df["wind"].apply(lambda x: x * 4)
+df.to_csv("wx_windy.csv", index=False)
