ac46c64b2ee96d80deababe056d6debba6241b8e
Jordan Evens <jordan.evens@nrcan-rncan.gc.ca>
2023-10-20 16:56:40 -0400

    add run.sh for running all languages on the same parameters

---
+++ run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+BIN=${@: 1:1}
+N=`expr $# - 3`
+ARGS=${@: 2:${N}}
+# input & output are always last 2 arguments
+INPUT=${@: -2:1}
+OUTPUT=${@: -1:1}
+OUTPUT_C=`echo $OUTPUT | sed "s/\.csv/.csv/g"`
+OUTPUT_PY=`echo $OUTPUT | sed "s/\.csv/_py.csv/g"`
+OUTPUT_R=`echo $OUTPUT | sed "s/\.csv/_r.csv/g"`
+LOG=`echo $OUTPUT | sed "s/\.csv/.log/g"`
+
+( \
+    echo Running ${BIN} for ${@: 2} \
+    && ./bin/${BIN} ${ARGS} ${INPUT} ${OUTPUT_C} > ${LOG} 2>&1 \
+    && python ${BIN}.py ${ARGS} ${INPUT} ${OUTPUT_PY} >> ${LOG} 2>&1 \
+    && Rscript ${BIN}.r ${ARGS} ${INPUT} ${OUTPUT_R} >> ${LOG} 2>&1 \
+) || (echo ERROR RUNNING: $* && exit -2)
+
+if [ -n "${CHECK_DIFF}" ]; then
+    (set -x diff ${OUTPUT_C} ${OUTPUT_PY}) || exit -1
+    (set -x diff ${OUTPUT_C} ${OUTPUT_R}) || exit -1
+fi;
