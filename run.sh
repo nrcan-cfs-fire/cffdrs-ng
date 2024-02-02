32438fbf74ae84d714072a58f8f0b7826d601818
Jordan Evens <jordan.evens@nrcan-rncan.gc.ca>
2024-02-02 09:53:55 -0500

    removed 'for' from line in run.sh saying what's running so line can be copied as-is when testing

--- run.sh
+++ run.sh
@@ -12,7 +12,7 @@ OUTPUT_PY=`echo $OUTPUT | sed "s/\.csv/_py.csv/g"`
 OUTPUT_R=`echo $OUTPUT | sed "s/\.csv/_r.csv/g"`
 LOG=`echo $OUTPUT | sed "s/\.csv/.log/g"`
 ( \
-    echo Running ${BIN} for ${@: 2} \
+    echo Running ${BIN} ${@: 2} \
     && ./bin/${BIN} ${ARGS} ${INPUT} ${OUTPUT_C} > ${LOG} 2>&1 \
     && python ${BIN}.py ${ARGS} ${INPUT} ${OUTPUT_PY} >> ${LOG} 2>&1 \
     && Rscript ${BIN}.r ${ARGS} ${INPUT} ${OUTPUT_R} >> ${LOG} 2>&1 \
