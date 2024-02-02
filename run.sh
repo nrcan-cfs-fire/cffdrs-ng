6f9ff512d24cd3eaedf7b7daa35291fb0f91eefe
Jordan Evens <jordan.evens@nrcan-rncan.gc.ca>
2024-02-02 07:50:53 -0500

    fixed run.sh not comparing files properly and test.sh not stopping on first failure

--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@
 BIN=${@: 1:1}
 N=`expr $# - 3`
 ARGS=${@: 2:${N}}
+# do after args check because that failing is fine
+set -e
 # input & output are always last 2 arguments
 INPUT=${@: -2:1}
 OUTPUT=${@: -1:1}
@@ -9,16 +11,14 @@ OUTPUT_C=`echo $OUTPUT | sed "s/\.csv/.csv/g"`
 OUTPUT_PY=`echo $OUTPUT | sed "s/\.csv/_py.csv/g"`
 OUTPUT_R=`echo $OUTPUT | sed "s/\.csv/_r.csv/g"`
 LOG=`echo $OUTPUT | sed "s/\.csv/.log/g"`
-
 ( \
     echo Running ${BIN} for ${@: 2} \
     && ./bin/${BIN} ${ARGS} ${INPUT} ${OUTPUT_C} > ${LOG} 2>&1 \
     && python ${BIN}.py ${ARGS} ${INPUT} ${OUTPUT_PY} >> ${LOG} 2>&1 \
     && Rscript ${BIN}.r ${ARGS} ${INPUT} ${OUTPUT_R} >> ${LOG} 2>&1 \
-) || (echo ERROR RUNNING: $* && exit -2)
-
+) || (echo ERROR RUNNING: $* && exit 1)
 if [ -n "${CHECK_DIFF}" ]; then
-    (set -x diff ${OUTPUT_C} ${OUTPUT_PY}) || (echo Python output does not match C && exit -1)
-    (set -x diff ${OUTPUT_C} ${OUTPUT_R}) || (echo R output does not match C && exit -1)
+    diff -q ${OUTPUT_C} ${OUTPUT_PY}
+    diff -q ${OUTPUT_C} ${OUTPUT_R}
     echo All files match ${OUTPUT_C}
 fi;
--- test.sh
+++ test.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
+set -e
+
 export CHECK_DIFF=$*
 python make_inputs.py
 
-cmake -S. -B./build || exit
-cmake --build ./build || exit
+cmake -S. -B./build
+cmake --build ./build
 
 mkdir -p ./out
 
-./run.sh NG_FWI -6 85 6 15 ./data/wx_hourly.csv ./out/wx_hourly_fwi.csv || exit
-./run.sh NG_FWI -6 85 6 15 ./data/test_hffmc.csv ./out/test_hffmc_fwi.csv || exit
+./run.sh NG_FWI -6 85 6 15 ./data/wx_hourly.csv ./out/wx_hourly_fwi.csv
+./run.sh NG_FWI -6 85 6 15 ./data/test_hffmc.csv ./out/test_hffmc_fwi.csv
 
-./run.sh make_daily ./data/wx_hourly.csv ./out/wx_daily.csv || exit
+./run.sh make_daily ./data/wx_hourly.csv ./out/wx_daily.csv
 
 # use same starting csv so any error in previous step doesn't carry forward
-./run.sh make_minmax ./out/wx_daily.csv ./out/wx_minmax.csv || exit
+./run.sh make_minmax ./out/wx_daily.csv ./out/wx_minmax.csv
 
-./run.sh make_hourly -6 ./out/wx_minmax.csv ./out/wx_diurnal.csv || exit
+./run.sh make_hourly -6 ./out/wx_minmax.csv ./out/wx_diurnal.csv
 
-./run.sh NG_FWI -6 85 6 15 ./out/wx_diurnal.csv ./out/wx_diurnal_fwi.csv || exit
+./run.sh NG_FWI -6 85 6 15 ./out/wx_diurnal.csv ./out/wx_diurnal_fwi.csv
 
-./run.sh NG_FWI -6 85 6 15 ./data/wx_windy.csv ./out/wx_windy_fwi.csv || exit
-./run.sh NG_FWI -6 85 6 15 ./data/wx_rh100.csv ./out/wx_rh100_fwi.csv || exit
-./run.sh NG_FWI -6 85 6 15 ./data/wx_rh0.csv ./out/wx_rh0_fwi.csv || exit
+./run.sh NG_FWI -6 85 6 15 ./data/wx_windy.csv ./out/wx_windy_fwi.csv
+./run.sh NG_FWI -6 85 6 15 ./data/wx_rh100.csv ./out/wx_rh100_fwi.csv
+./run.sh NG_FWI -6 85 6 15 ./data/wx_rh0.csv ./out/wx_rh0_fwi.csv
