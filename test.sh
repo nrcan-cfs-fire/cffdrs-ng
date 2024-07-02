ce2478e5e7fea3c2915e11e3de54b69755e01390
Jordan Evens <jordan.evens@nrcan-rncan.gc.ca>
2024-07-02 11:40:06 -0400

    add FIX comment before tests that don't produce same outputs

--- test.sh
+++ test.sh
@@ -10,12 +10,15 @@ cmake --build ./build
 mkdir -p ./out
 
 ./run.sh make_inputs -6 ./data/wx_hourly.csv ./out/inputs_hourly.csv
+# FIX
 ./run.sh NG_FWI -6 85 6 15 ./out/inputs_hourly.csv ./out/wx_hourly_fwi.csv
 
 ./run.sh make_inputs -6 ./data/test_hffmc.csv ./out/inputs_hffmc.csv
+# FIX
 ./run.sh NG_FWI -6 85 6 15 ./out/inputs_hffmc.csv ./out/test_hffmc_fwi.csv
 
 ./run.sh make_inputs -6 ./data/test_hffmc.csv ./out/inputs_hffmc.csv
+# FIX
 ./run.sh NG_FWI -6 85 6 15 ./out/inputs_hffmc.csv ./out/test_hffmc_fwi.csv
 
 ./run.sh make_daily ./data/wx_hourly.csv ./out/wx_daily.csv
@@ -27,13 +30,17 @@ mkdir -p ./out
 ./run.sh make_hourly -6 ./out/wx_minmax.csv ./out/wx_diurnal.csv
 
 ./run.sh make_inputs -6 ./out/wx_diurnal.csv ./out/inputs_diurnal.csv
+# FIX
 ./run.sh NG_FWI -6 85 6 15 ./out/inputs_diurnal.csv ./out/wx_diurnal_fwi.csv
 
 ./run.sh make_inputs -6 ./data/wx_windy.csv ./out/inputs_windy.csv
+# FIX
 ./run.sh NG_FWI -6 85 6 15 ./out/inputs_windy.csv ./out/wx_windy_fwi.csv
 
 ./run.sh make_inputs -6 ./data/wx_rh100.csv ./out/inputs_rh100.csv
+# FIX
 ./run.sh NG_FWI -6 85 6 15 ./out/inputs_rh100.csv ./out/wx_rh100_fwi.csv
 
 ./run.sh make_inputs -6 ./data/wx_rh0.csv ./out/inputs_rh0.csv
+# FIX
 ./run.sh NG_FWI -6 85 6 15 ./out/inputs_rh0.csv ./out/wx_rh0_fwi.csv
